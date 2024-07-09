dce39ff6c555771d2bb67343cea726a4c5bd8f97
21701A05E0 <rraj6170825@gmail.com>
2024-07-09 19:02:52 +0530

    Add files via upload
    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Registration from './Registration';
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <Registration />
+        
+      </div>
+      
+    </>
+  )
+}
+
+export default App
